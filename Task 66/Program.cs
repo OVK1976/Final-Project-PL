7530303b4d3c444ec3ace01ed6bd9d1db00187cf
OVK1976 <olegkalinin@yandex.ru>
2022-11-07 11:27:35 +0400

    Final Version

--- Task 66/Program.cs
+++ Task 66/Program.cs
@@ -5,3 +5,29 @@
 
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30 */
+
+Console.WriteLine("Введите значение M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение N");
+int n = int.Parse(Console.ReadLine());
+
+    if (m < n)
+    {
+        Console.WriteLine($"Сумма элементов от {m} до {n} равна: {SumFromMToN(m, n)}");
+    }
+    else
+    {
+        Console.WriteLine($"Сумма элементов от {n} до {m} равна:{SumFromMToN(n, m)}");
+    }
+
+#region Сумма элементов от M до N
+int SumFromMToN(int m, int n)
+{
+    int SumNum = 0;
+    for (int i = m; i <= n; i++)
+    {
+        SumNum += i;
+    }
+    return SumNum;
+}
+#endregion
--- Task 68/Program.cs
+++ Task 68/Program.cs
@@ -1,3 +1,28 @@
 ﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 2, n = 3->A(m, n) = 9
 m = 3, n = 2->A(m, n) = 29 */
+Console.WriteLine("Введите положительное число m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите положительное число n");
+int n = int.Parse(Console.ReadLine());
+Console.Write($"Функция Аккермана равна: {AkkermanFunction(m, n)}");
+
+// функция Аккермана
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if (n == 0 && m > 0)
+        {
+            return AkkermanFunction(m - 1, 1);
+        }
+        else
+        {
+            return (AkkermanFunction(m - 1, AkkermanFunction(m, n - 1)));
+        }
+    }
+}
